609a0a9aef7f3808f270c11a8cb7c8b4fa96138d
Bishal Thapaliya <bishalthapaliya@Bishals-MacBook-Pro.local>
2026-09-03 10:43:22 +0200

    feat: add file reading functionality

--- file_practice.py
+++ file_practice.py
@@ -1,7 +1,16 @@
+# an array of str
 test_results = ["login test: PASS", "sort test: FAIL", "checkout test: PASS"]
 
+# writing to a file
 with open("results.txt", "w") as file:
     for result in test_results:
         file.write(result + "\n")
 
 print("File written successfully")
+
+# reading a file
+with open("results.txt", "r") as file:
+    lines = file.readlines()
+
+for line in lines:
+    print(line)
